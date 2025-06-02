ff27621ec66e175a333b2b3d0757007a994ef346
Ryan Lima Ferreira <ry90ryan@gmail.com>
2025-06-02 08:44:34 -0300

    Update and rename while_tabuada.java to WhileTabuada.java

--- WhileTabuada.java
+++ WhileTabuada.java
@@ -2,7 +2,7 @@ package src;
 
 import java.util.Scanner;
 
-public class while_tabuada {
+public class WhileTabuada {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
